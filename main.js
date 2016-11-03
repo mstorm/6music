54f4bad8ea9172d1326a25e5f2bb2e19938767e8
Stephen Kwon <mstorm@mstorm.net>
2016-11-03 15:43:15 +0900

    Support flash player plugins.

--- main.js
+++ main.js
@@ -1,30 +1,47 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let pluginName
+switch (process.platform) {
+  case 'win32':
+    pluginName = 'pepflashplayer.dll'
+    break
+  case 'darwin':
+    pluginName = 'PepperFlashPlayer.plugin'
+    break
+  case 'linux':
+    pluginName = 'libpepflashplayer.so'
+    break
+}
+
+app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName));
+app.commandLine.appendSwitch('ppapi-flash-version', '18.0.0.203');
+
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
+  const width = 380;
+  const height = 665;
+
   mainWindow = new BrowserWindow({
     show: false,
-     width: 380,
-     height: 665,
-     useContentSize: true,
-     toolbar: false,
-     resizeable: false
-   })
+    width: width,
+    height: height,
+    useContentSize: true,
+    toolbar: false,
+    resizeable: false,
+    'web-preferences': {'plugins': true}
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/app/index.html')
+  var url = 'file://' + __dirname + '/app/index.html';
+  //url = 'http://www.mstorm.net/toy/user-agent';
+  mainWindow.loadURL(url);
 
   mainWindow.once('ready-to-show', () => {
     mainWindow.show()
-  })
+  });
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -34,14 +51,14 @@ function createWindow () {
     mainWindow = null
     app.quit()
   })
-}
+};
 
-app.setName("BBC 6music")
+app.setName("BBC 6music");
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -50,7 +67,7 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
@@ -58,7 +75,7 @@ app.on('activate', function () {
   if (mainWindow === null) {
     createWindow()
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
